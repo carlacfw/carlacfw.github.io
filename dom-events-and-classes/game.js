9e27282829061d1ad380449e9df86164cc452b95
carla <carlacfw@gmail.com>
2017-06-01 16:45:38 +1200

    hide function added

--- dom-events-and-classes/game.js
+++ dom-events-and-classes/game.js
@@ -13,6 +13,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('click', makeBlue)
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -30,6 +31,12 @@ updateCounts()
 }
 
 // CREATE FUNCTION hide HERE
+function hide (evt) {
+evt.target.classList.toggle('invisible')
+updateCounts()
+
+}
+
 
 function updateCounts () {
   var totals = {
